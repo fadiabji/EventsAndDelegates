acf2c63fb1cdfc4bfe81d729ce938ef7b5d31986
Fadi abji <fadi.abji@hotmail.com>
2024-06-04 21:19:01 +0200

    Add project files.

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EventsAndDelegates;
+
+Console.WriteLine("start the app!");
+
+
+var video = new Video() { Title = "Video 1" };
+
+var videoEncoder  = new VideoEncoder(); // publisher
+
+var mailService = new MailService();   // subscriber
+
+var messageService = new MessageService(); // subscriber
+
+videoEncoder.VideoEncoded += mailService.onVideoEncoded; 
+videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+videoEncoder.Encode(video);
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class VideoEncoder
+    {
+        // we want an event in this class to notify the user that the video is finnish 
+        // Three steps we need to follow:
+
+        // 1 - define a delegate, contract between the publisher and the subscriper
+        //public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+
+        // 2 - define an event based on that delegate, notice it is in the past tens something that happend and finnish
+        //public event VideoEncodedEventHandler VideoEncoded;
+
+        // 1 and 2 you can make them simply as here
+        //Instead of writing two lines of code here we could simply write
+        //EventHandler (withot sending data) or EventHandler<TEventArgs> (sending some data) like 
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+
+        public void Encode(Video vedio)
+        {
+            Console.WriteLine("Encoding vedio...");
+            Thread.Sleep(3000);
+            OnVideoEncoded(vedio);
+        }
+
+        // 3 - Raise the event
+        // This method is responsable to notify the subscripers that something happends
+        protected virtual void OnVideoEncoded(Video video) 
+        {
+            if(VideoEncoded != null) 
+            {
+                VideoEncoded(this, new VideoEventArgs() { Video = video});
+            }
+
+        }
+    }
+}
